a23ccdc56175a8e7d1a7c87feb19990fb3e06c99
TopVirusMinus <mwmma5000@gmail.com>
2022-11-09 00:28:37 +0200

    added add-delete-enter cost shortcuts

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,44 @@
-import uvicorn
+from typing import List
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
-if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+app = FastAPI()
+
+origins = [
+    "http://localhost:5500",
+    "localhost:5500",
+    "http://127.0.0.1:5500/"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+nodes = {}
+edges = {}
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Graph Path Finding Visualizer"}
+
+
+class BaseParam(BaseModel):
+    nodes: List[dict]
+    edges: List[dict]
+
+
+@app.post("/receiveInfo/", status_code=201)
+async def receiveInfo(baseParam: BaseParam):
+    res = baseParam
+
+    nodes = res.nodes
+    edges = res.edges
+    print(res)
--- network.js
+++ network.js
@@ -1,154 +1,213 @@
 // create an array with nodes
 var nodes = new vis.DataSet([
-    { id: 1, label: "Node 1", }
+  { id: 1, label: "Start" },
+  { id: 2, label: "Node 1" },
+  { id: 3, label: "Node 2" },
+  { id: 4, label: "Node 3" },
+  { id: 5, label: "end" },
 ]);
 
 // create an array with edges
 var edges = new vis.DataSet([
+  { from: 1, to: 2, label: "2" },
+  { from: 1, to: 3, label: "5" },
+  { from: 2, to: 3, label: "3" },
+  { from: 2, to: 4, label: "1" },
+  { from: 3, to: 4, label: "3" },
+  { from: 4, to: 5, label: "2" },
 ]);
+
 var selectednode = 0;
+var lastNodeNum = 3;
+
 // create a network
 var container = document.getElementById("mynetwork");
 var data = {
-    nodes: nodes,
-    edges: edges,
+  nodes: nodes,
+  edges: edges,
 };
 
 var options = {
-    interaction: { hover: true },
-    manipulation: {
-        enabled: true,
+  interaction: { hover: true },
+  manipulation: {
+    enabled: true,
+    addNode: function (nodeData, callback) {
+      lastNodeNum += 1;
+      nodeData.label = `Node ${lastNodeNum}`;
+      callback(nodeData);
     },
+    addEdge: function (edgeData, callback) {
+      if (edgeData.from === edgeData.to) {
+        var r = confirm("Do you want to connect the node to itself?");
+        if (r === true) {
+          callback(edgeData);
+        }
+      } else {
+        callback(edgeData);
+      }
+    },
+  },
 };
+
+document.getElementById("visualize").addEventListener("click", async () => {
+  axios
+    .post("http://localhost:8000/receiveInfo/", {
+      nodes: nodes.get(),
+      edges: edges.get(),
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+});
 
 var network = new vis.Network(container, data, options);
 var ct = 0;
+
 network.on("click", function (params) {
+  params.event = "[original event]";
+  document.getElementById("eventSpanHeading").innerText = "Click event:";
+  document.getElementById("eventSpanContent").innerText = JSON.stringify(
+    params,
+    null,
+    4
+  );
 
-    params.event = "[original event]";
-    document.getElementById("eventSpanHeading").innerText = "Click event:";
-    document.getElementById("eventSpanContent").innerText = JSON.stringify(
-        params,
-        null,
-        4
-    );
-    console.log(
-        "click event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM)
-    );
-    selectednode = this.getNodeAt(params.pointer.DOM);
-    if (ct % 2 == 0) {
-        startnode = this.getNodeAt(params.pointer.DOM);
-    } else {
-        endnode = this.getNodeAt(params.pointer.DOM);
-    }
-    ct++;
-    console.log(selectednode);
-    console.log(nodes.get());
-    console.log(edges.get());
-    // nodes.update({ id: 2, label: "item 2 (new)" });
+  console.log("nodes: ", data.nodes.get());
+  console.log("edges: ", data.edges.get());
 
-});
-network.on("doubleClick", function (params) {
-    // var edgesIDs = properties.edges;
-    // clickedEdges = edges.get(edgesIDs);
-    // console.log(clickedEdges + "000000000000000000");
-    params.event = "[original event]";
-    document.getElementById("eventSpanHeading").innerText = "doubleClick event:";
-    document.getElementById("eventSpanContent").innerText = JSON.stringify(
-        params,
-        null,
-        4
-    );
+  console.log(
+    "click event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM)
+  );
+  selectednode = this.getNodeAt(params.pointer.DOM);
+  if (ct % 2 == 0) {
+    startnode = this.getNodeAt(params.pointer.DOM);
+  } else {
     endnode = this.getNodeAt(params.pointer.DOM);
+  }
+  ct++;
+  //   console.log(selectednode);
+  //   console.log(nodes.get());
+  //   console.log(edges.get());
+  // nodes.update({ id: 2, label: "item 2 (new)" });
+});
+
+network.on("doubleClick", function (params) {
+  // var edgesIDs = properties.edges;
+  // clickedEdges = edges.get(edgesIDs);
+  // console.log(clickedEdges + "000000000000000000");
+  params.event = "[original event]";
+  document.getElementById("eventSpanHeading").innerText = "doubleClick event:";
+  document.getElementById("eventSpanContent").innerText = JSON.stringify(
+    params,
+    null,
+    4
+  );
+  endnode = this.getNodeAt(params.pointer.DOM);
+});
+
+network.on("click", (params) => {
+  if (params.nodes.length == 0 && params.edges.length == 0) {
+    console.log("add new node!");
+    lastNodeNum += 1;
+    var updatedIds = nodes.add([
+      {
+        label: `Node ${lastNodeNum}`,
+        x: params.pointer.canvas.x,
+        y: params.pointer.canvas.y,
+      },
+    ]);
+  }
 });
 var counter = 1;
 function updateedge() {
-
-    var cost = document.getElementById("cost").value;
-    console.log(heu);
-    edges.add([{ id: counter, from: startnode, to: endnode, label: cost }])
-    counter++;
+  var cost = document.getElementById("cost").value;
+  console.log(heu);
+  edges.add([{ id: counter, from: startnode, to: endnode, label: cost }]);
+  counter++;
 }
 network.on("oncontext", function (params) {
-    params.event = "[original event]";
-    document.getElementById("eventSpanHeading").innerText =
-        "oncontext (right click) event:";
-    document.getElementById("eventSpanContent").innerText = JSON.stringify(
-        params,
-        null,
-        4
-    );
+  params.event = "[original event]";
+  document.getElementById("eventSpanHeading").innerText =
+    "oncontext (right click) event:";
+  document.getElementById("eventSpanContent").innerText = JSON.stringify(
+    params,
+    null,
+    4
+  );
 });
 var startnode = 0;
 var endnode = 0;
 network.on("dragStart", function (params) {
-    // There's no point in displaying this event on screen, it gets immediately overwritten
-    params.event = "[original event]";
-    console.log("dragStart Event:", params);
-    console.log(
-        "dragStart event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM)
-    );
-
+  // There's no point in displaying this event on screen, it gets immediately overwritten
+  params.event = "[original event]";
+  console.log("dragStart Event:", params);
+  console.log(
+    "dragStart event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM)
+  );
 });
+
 network.on("dragging", function (params) {
-    params.event = "[original event]";
-    document.getElementById("eventSpanHeading").innerText = "dragging event:";
-    document.getElementById("eventSpanContent").innerText = JSON.stringify(
-        params,
-        null,
-        4
-    );
+  params.event = "[original event]";
+  document.getElementById("eventSpanHeading").innerText = "dragging event:";
+  document.getElementById("eventSpanContent").innerText = JSON.stringify(
+    params,
+    null,
+    4
+  );
 });
+
 function updatenode() {
-    var txt = document.getElementById("nname").value;
-    var heu = document.getElementById("heu").value;
-    console.log(heu);
-    nodes.updateOnly({ id: selectednode, label: txt, title: heu });
-
+  var txt = document.getElementById("nname").value;
+  var heu = document.getElementById("heu").value;
+  console.log(heu);
+  nodes.updateOnly({ id: selectednode, label: txt, title: heu });
 }
-network.on("dragEnd", function (params) {
-    params.event = "[original event]";
-    document.getElementById("eventSpanHeading").innerText = "dragEnd event:";
-    document.getElementById("eventSpanContent").innerText = JSON.stringify(
-        params,
-        null,
-        4
-    );
-    console.log("dragEnd Event:", params);
-    console.log(
-        "dragEnd event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM)
-    );
 
+network.on("dragEnd", function (params) {
+  params.event = "[original event]";
+  document.getElementById("eventSpanHeading").innerText = "dragEnd event:";
+  document.getElementById("eventSpanContent").innerText = JSON.stringify(
+    params,
+    null,
+    4
+  );
+  //console.log("dragEnd Event:", params);
+  //console.log(
+  //"dragEnd event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM)
+  //);
 });
+
 network.on("controlNodeDragging", function (params) {
-    params.event = "[original event]";
-    document.getElementById("eventSpanHeading").innerText =
-        "control node dragging event:";
-    document.getElementById("eventSpanContent").innerText = JSON.stringify(
-        params,
-        null,
-        4
-    );
+  params.event = "[original event]";
+  document.getElementById("eventSpanHeading").innerText =
+    "control node dragging event:";
+  document.getElementById("eventSpanContent").innerText = JSON.stringify(
+    params,
+    null,
+    4
+  );
 });
+
 network.on("controlNodeDragEnd", function (params) {
-    params.event = "[original event]";
-    document.getElementById("eventSpanHeading").innerText =
-        "control node drag end event:";
-    document.getElementById("eventSpanContent").innerText = JSON.stringify(
-        params,
-        null,
-        4
-    );
-    console.log("controlNodeDragEnd Event:", params);
+  params.event = "[original event]";
+  document.getElementById("eventSpanHeading").innerText =
+    "control node drag end event:";
+  document.getElementById("eventSpanContent").innerText = JSON.stringify(
+    params,
+    null,
+    4
+  );
+  //console.log("controlNodeDragEnd Event:", params);
 });
+
 network.on("zoom", function (params) {
-    // document.getElementById("eventSpanHeading").innerText = "zoom event:";
-    //document.getElementById("eventSpanContent").innerText = JSON.stringify(
-    //   params,
-    //  null,
-    // 4
-    //);
+  // document.getElementById("eventSpanHeading").innerText = "zoom event:";
+  //document.getElementById("eventSpanContent").innerText = JSON.stringify(
+  //   params,
+  //  null,
+  // 4
+  //);
 });
+
 // network.on("showPopup", function (params) {
 //     document.getElementById("eventSpanHeading").innerText = "showPopup event: ";
 //     document.getElementById("eventSpanContent").innerText = JSON.stringify(
@@ -157,34 +216,47 @@ network.on("zoom", function (params) {
 //         4
 //     );
 // });
+
+container.addEventListener("keydown", (e) => {
+  if (e.key === "Delete") {
+    network.deleteSelected();
+  } else if (e.key === "Enter") {
+    let cost = prompt("Enter Cost");
+    edges.updateOnly({ id: selectededge, label: cost, title: heu });
+    console.log(`edit edge cost ${selectededge}`);
+  }
+});
+
 network.on("hidePopup", function () {
-    console.log("hidePopup Event");
+  //console.log("hidePopup Event");
 });
 network.on("select", function (params) {
-    console.log("select Event:", params);
+  //console.log("select Event:", params);
 });
 network.on("selectNode", function (params) {
-    console.log("selectNode Event:", params);
+  selectednode = params.nodes[0];
+  console.log(selectednode);
+  var node = network.body.nodes[selectednode];
 });
 network.on("selectEdge", function (params) {
-    console.log("selectEdge Event:", params);
-    selectededge = this.getEdgeAt(params.pointer.DOM);
+  console.log("selectEdge Event:", params);
+  selectededge = this.getEdgeAt(params.pointer.DOM);
 });
 network.on("deselectNode", function (params) {
-    console.log("deselectNode Event:", params);
+  //console.log("deselectNode Event:", params);
 });
 network.on("deselectEdge", function (params) {
-    console.log("deselectEdge Event:", params);
+  //console.log("deselectEdge Event:", params);
 });
 network.on("hoverNode", function (params) {
-    console.log("hoverNode Event:", params);
+  //console.log("hoverNode Event:", params);
 });
 network.on("hoverEdge", function (params) {
-    console.log("hoverEdge Event:", params);
+  //console.log("hoverEdge Event:", params);
 });
 network.on("blurNode", function (params) {
-    console.log("blurNode Event:", params);
+  //console.log("blurNode Event:", params);
 });
 network.on("blurEdge", function (params) {
-    console.log("blurEdge Event:", params);
+  //console.log("blurEdge Event:", params);
 });

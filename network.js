4f8d31bb6250b90b3abf6b4c8803933cb9021a8d
TopVirusMinus <mwmma5000@gmail.com>
2022-11-13 21:35:11 +0200

    renamed nodes

--- network.js
+++ network.js
@@ -1,10 +1,10 @@
 // create an array with nodes
 var nodes = new vis.DataSet([
-  { id: 1, label: "Start" },
-  { id: 2, label: "Node 1" },
-  { id: 3, label: "Node 2" },
-  { id: 4, label: "Node 3" },
-  { id: 5, label: "end" },
+  { id: 1, label: "Node 1" },
+  { id: 2, label: "Node 2" },
+  { id: 3, label: "Node 3" },
+  { id: 4, label: "Node 4" },
+  { id: 5, label: "Node 5" },
 ]);
 
 // create an array with edges

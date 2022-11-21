8c132902905e4dcb674ac04ada8a26d196dbd686
TopVirusMinus <mwmma5000@gmail.com>
2022-11-22 00:42:35 +0200

    added A* algorithm, and fixed visited list

--- backend/main.py
+++ backend/main.py
@@ -29,6 +29,7 @@ graph = defaultdict(list)
 algorithm = "bfs"
 source = -1
 destination = set()
+heuristics = {}
 
 def bfs():
     global source, destination
@@ -39,6 +40,7 @@ def bfs():
     queue.append(source)
     
     visited = set()
+    visitedList = []
     shortest_path = []
     fringe = []
     
@@ -46,8 +48,9 @@ def bfs():
         fringe.append(queue.copy())    
         curr = queue.pop(0)
         visited.add(curr)
+        visitedList.append(curr)
         print(curr)
-        for c,d,h in graph[curr]:
+        for c,d in graph[curr]:
             if c not in visited:
                 queue.append(c)
                 visited.add(c)
@@ -69,7 +72,7 @@ def bfs():
                 print("shorest path",shortest_path)
                 print("visited", visited)
                 print("fringe", fringe)
-                return shortest_path, visited, fringe
+                return shortest_path, visitedList, fringe
             
     return {"msg":"bfs algorithm"}
 
@@ -81,6 +84,7 @@ def uniform_cost():
     priority_queue = []
     backtrack = {}
     visited = set()
+    visitedList = []
     shortest_path = []
     fringe = []
     
@@ -94,6 +98,7 @@ def uniform_cost():
         curr, cost = priority_queue.pop(0)
         #print(curr)
         visited.add((curr,cost))
+        visitedList.append((curr,cost))
         
         if curr in destination:
             print("found!")
@@ -110,21 +115,62 @@ def uniform_cost():
             print("shorest path",shortest_path)
             #print("visited", visited)
             #print("fringe", fringe)
-            return shortest_path, visited, fringe
+            return shortest_path, visitedList, fringe
         
-        for n,c,h in graph[curr]:
+        for n,c in graph[curr]:
             if (n, c+cost) not in visited:
                 backtrack[n] = curr
                 visited.add((n, c+cost))
+                visitedList.append((n,c+cost))
                 priority_queue.append((n, c+cost))
                 
         priority_queue = sorted(priority_queue,key=lambda t: t[1])
     return {"msg":"uniform cost algorithm"}
 
 def greedy_best_first():
+    
     return {"msg":"best first algorithm"}
 
 def a_star():
+    global source, destination
+    visited = set()
+    visitedList = []
+    queue = [(source,0)]
+    fringe = []
+    backtrack = {}
+    shortest_path = []
+
+    while queue:
+        queue.sort(key=lambda t: t[1])  #to sort according f-cost ystaa
+        fringe.append(queue.copy())
+        print(queue)
+
+        node, cost = queue.pop(0)  #a5tar 2a2l f-cost\
+        print(node)
+
+        if node in visited:
+            continue
+
+        visited.add(node)
+        visitedList.append(node)
+        if node in destination:
+            new_destination = node
+            #print(backtrack)
+            while backtrack[new_destination] != source:
+                #print(new_destination)
+                new_destination = backtrack[new_destination]
+                shortest_path.append(new_destination)
+
+            shortest_path = shortest_path[::-1]
+            shortest_path.insert(0, source)
+            shortest_path.append(node)
+            return shortest_path, fringe, visitedList
+
+        for node2, cost2 in graph[node]:
+            if node2 not in visited:
+                backtrack[node2] = node
+                costXheu = (cost - heuristics[node]) + cost2 + heuristics[node2]
+                queue.append((node2, costXheu))
     return {"msg":"A* algorithm"}
 
 algorithms = {"bfs": bfs, "dfs":dfs, "uniformCost":uniform_cost, "greedyBestFirst":greedy_best_first, "A*":a_star}
@@ -148,9 +194,10 @@ class BaseParam(BaseModel):
 
 @app.post("/receiveInfo/", status_code=201)
 async def receiveInfo(baseParam: BaseParam):
-    global nodes, edges, graph, algorithm, source, destination
+    global nodes, edges, graph, algorithm, source, destination,heuristics
     nodes = {}
     edges = {}
+    heuristics = {}
     graph = defaultdict(list)
     algorithm = "bfs"
     source = -1                                                                                 
@@ -162,18 +209,18 @@ async def receiveInfo(baseParam: BaseParam):
     algorithm = res.algorithm
     source = res.source
     
+    print("SOURCE", source)
     heuristics = defaultdict(lambda: -1)
     
     for n in nodes:
         heuristics[n["id"]] = int(n["title"])
 
-    #print(heuristics)
     for d in res.destination:
         destination.add(d)
         
     for edge in edges:
         h = heuristics[edge["to"]]
-        graph[edge["from"]].append((edge["to"],int(edge["label"]), h)) 
+        graph[edge["from"]].append((edge["to"],int(edge["label"]))) 
 
     print(algorithm)
     print(graph)
